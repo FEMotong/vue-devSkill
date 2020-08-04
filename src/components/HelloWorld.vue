22aa0e7318258421714ac99b3e6f167b1f9c8804
MoTong <17788730976@189.cn>
2020-08-04 12:06:01 +0800

    优雅更新props

---
+++ src/components/HelloWorld.vue
@@ -0,0 +1,42 @@
+<template>
+  <div class="hello">
+    <h1>{{ msg }}</h1>
+    <h2>作者：{{ Author }}</h2>
+  </div>
+</template>
+
+<script>
+export default {
+  name: 'HelloWorld',
+  props: {
+    msg: String,
+    Author: String
+  },
+  created() {
+    this.changAuthor()
+  },
+  methods: {
+    changAuthor() {
+      this.$emit("update:Author", "莫童高级前端架构师")
+    }
+  }
+}
+</script>
+
+<!-- Add "scoped" attribute to limit CSS to this component only -->
+<style scoped>
+h3 {
+  margin: 40px 0 0;
+}
+ul {
+  list-style-type: none;
+  padding: 0;
+}
+li {
+  display: inline-block;
+  margin: 0 10px;
+}
+a {
+  color: #42b983;
+}
+</style>
---
+++ src/views/Home.vue
@@ -0,0 +1,23 @@
+<template>
+  <div class="home">
+    <HelloWorld :Author.sync="Author" msg="关于Vue的开发技巧！"/>
+    <h3>{{ Author }}</h3>
+  </div>
+</template>
+
+<script>
+// @ is an alias to /src
+import HelloWorld from '@/components/HelloWorld.vue'
+
+export default {
+  name: 'Home',
+  components: {
+    HelloWorld
+  },
+  data() {
+    return {
+      Author: ''
+    }
+  }
+}
+</script>
